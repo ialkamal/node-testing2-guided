d384b963ae5d0f72de93021d3fedefe5e52031a4
Ismail Al Kamal <ismail@nawatt.com>
2021-02-18 07:02:57 +0300

    production

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,36 @@
+const pgConnection = process.env.DATABASE_URL;
+
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3",
     },
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3",
     },
   },
   production: {
-
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
 };
